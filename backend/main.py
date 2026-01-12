84ea53fd0025ee41d61c1bcdae739b3d911ceec6
dbourene <dbourene@audencia.com>
2026-01-12 21:50:21 +0100

    Ajout dans main.py

--- backend/main.py
+++ backend/main.py
@@ -5,4 +5,9 @@ from fastapi import FastAPI
 from routes import geocode
 
 app = FastAPI()
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
 app.include_router(geocode.router)
